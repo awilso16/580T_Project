e4287906237ca0fb116352cb3476b793e7785080
Adam Wilson <wilso@Adams-MacBook-Pro.local>
2022-11-30 22:12:14 -0500

    forgot to push WPA2 earlier, pushing now

--- 2auth.py
+++ 2auth.py
@@ -1,20 +1,33 @@
 import socket 
+from cryptography.fernet import Fernet
 
 HOST = '127.0.0.1'
 PORT = 5000
 
-password = 'password'
+password = "password"
+STA_MAC = 'E8:61:09:B9:75:60'
+AP_MAC = '48:C9:4D:AA:31:47'
+
+gtk = int('26DC5C6CE94A4B44F330B5D9BBD77CBF958416295CF7E1CE6BCCDC18FF8C07B6', 16)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     print("listening on port #",PORT)
     conn, addr = s.accept()
+    anonce = int('A9FB57DBA1EEA9BC3E660A909D838D726E3BF623D52620282013481D1F6E5377', 16) #random 32 byte number
+    anonce_bytes = anonce.to_bytes(32,byteorder='big')
+    conn.sendall(anonce_bytes)
     #can send using conn.sendall
     with conn:
         while True:
             buf = b''
-            #receive using conn.recv
-
+            buf += conn.recv(32)
+            snonce = int.from_bytes(buf,'big')
+            str_anonce = str(anonce)
+            str_snonce = str(snonce)
+            ptk = password + str_anonce + str_snonce + AP_MAC + STA_MAC
+            print(ptk, gtk)
+            conn.sendall(gtk.to_bytes(32, byteorder='big'))
             break
     s.close()
--- 2supp.py
+++ 2supp.py
@@ -1,10 +1,27 @@
 import socket 
+from cryptography.fernet import Fernet
 HOST = '127.0.0.1'
 PORT = 5000
 
-password = 'password'
+password = "password"
+STA_MAC = 'E8:61:09:B9:75:60'
+AP_MAC = '48:C9:4D:AA:31:47'
+
+snonce = int('7D5A0975FC2C3057EEF67530417AFFE7FB8055C126DC5C6CE94A4B44F330B5D9', 16) #random 32 byte int
+str_snonce = str(snonce)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     print("connected!")
+    buf = b''
+    buf += s.recv(64)
+    anonce = int.from_bytes(buf, 'big')
+    #PTK = PMK + ANONCE + SNONCE + MAC(AA) + MAC(SA)
+    str_anonce = str(anonce)
+    ptk = password + str_anonce + str_snonce + AP_MAC + STA_MAC
+    s.sendall(snonce.to_bytes(32,byteorder='big'))
+    buf2 = b''
+    buf2 += s.recv(64)
+    gtk = int.from_bytes(buf2,'big')
+    print(ptk, gtk)
     #can send using s.sendall and receive using s.recv
